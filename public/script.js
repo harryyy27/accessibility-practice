83d1615042fd206e987205b9972ab7938c2b2b9e
harryyy27 <harryyy27@gmail.com>
2018-12-12 22:54:27 +0000

    collisions added

--- public/script.js
+++ public/script.js
@@ -15,41 +15,66 @@ canvas.height = window.innerHeight;
 console.log(HTMLCanvasElement);
 var c = canvas.getContext('2d');
 
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// c.fillRect(100, 200, 100, 100);
-// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-// c.fillRect(400, 200, 100, 100);
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-// c.fillRect(300, 400, 100, 100);
+function distance(x1,y1,x2,y2){
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
+    return Math.sqrt(Math.pow(xDist,2)+Math.pow(yDist,2));
+}
+function rotate(velocity, angle) {
+    const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
 
-// //line
-// c.beginPath()
-// c.moveTo(50, 400);
-// c.lineTo(300, 200);
-// c.lineTo(400, 400);
-// c.strokeStyle = '#fa34a3'
-// c.stroke();
+    return rotatedVelocities;
+}
 
-//arc
-// c.beginPath();
-// c.arc(300, 400, 30, 0, Math.PI*2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
+/**
+ * Swaps out two colliding particles' x and y velocities after running through
+ * an elastic collision reaction equation
+ *
+ * @param  Object | particle      | A particle object with x and y coordinates, plus velocity
+ * @param  Object | otherParticle | A particle object with x and y coordinates, plus velocity
+ * @return Null | Does not return a value
+ */
 
-// for(i=0; i<200; i++) {
-//     var x = Math.random()* window.innerWidth;
-//     var y = Math.random() *window.innerHeight;
-//     var color1 = Math.floor(Math.random()*255).toString(16);
-//     var color2 = Math.floor(Math.random()*255).toString(16);
-//     var color3 = Math.floor(Math.random()*255).toString(16);
-//     var color = '#'+color1+color2+color3;
-//     console.log(color);
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI*2, false);
-//     c.strokeStyle = color;
-//     c.stroke();
-// }
+function resolveCollision(particle, otherParticle) {
+    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
 
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
+
+        // Velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherParticle.velocity, angle);
+
+        // Velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherParticle.velocity.x = vFinal2.x;
+        otherParticle.velocity.y = vFinal2.y;
+    }
+}
 //animating circle
 var mouse = {
     x: undefined,
@@ -62,14 +87,17 @@ window.addEventListener('mousemove', function(event){
     mouse.y=event.y;
 })
 
-function Circle(x,y,dx,dy,radius,color) {
+function Circle(x,y,radius,color) {
     this.x = x;
     this.y= y;
-    this.dx= dx;
-    this.dy = dy;
+    this.velocity = {
+        x:(Math.random()-0.5)*5,
+        y:(Math.random()-0.5)*5,
+    };
+   
     this.ddx=0;
     this.ddy=0;
-
+    this.mass = 1;
     this.radius = radius;
     this.minRadius = radius;
     this.color = color;
@@ -80,14 +108,23 @@ function Circle(x,y,dx,dy,radius,color) {
     c.fill();
     }
     this.update = function() {
+        for(let i = 0; i<circleArray.length; i++){
+            if(distance(this.x,this.y,circleArray[i].x,circleArray[i].y)!==0){
+                    if(distance(this.x,this.y,circleArray[i].x,circleArray[i].y)<circleArray[i].radius+this.radius){
+                       resolveCollision(this, circleArray[i])
+                    }
+                
+                }
+
+        }
         if(this.x>(innerWidth-this.radius) || this.x<(0+this.radius)){
-            this.dx =-this.dx;
+            this.velocity.x =-this.velocity.x;
             }
             if(this.y>(innerHeight-this.radius) || this.y<(0+this.radius)){
-                this.dy=-this.dy;
+                this.velocity.y=-this.velocity.y;
                 }
-            this.x+=this.dx;
-            this.y+=this.dy;
+            this.x+=this.velocity.x;
+            this.y+=this.velocity.y;
             //gravity mouse??
             
         if(Math.pow(Math.pow(mouse.x-this.x,2)+Math.pow(mouse.y-this.y,2),0.5 )<200){
@@ -101,8 +138,8 @@ function Circle(x,y,dx,dy,radius,color) {
             console.log(this.y);
             this.ddx=1*(this.x-mouse.x)/Math.pow(Math.pow(mouse.x-this.x,2)+Math.pow(mouse.y-this.y,2),0.5 );
             }
-            this.dx-=this.ddx;
-            this.dy-=this.ddy;
+            this.velocity.x-=this.ddx;
+            this.velocity.x-=this.ddy;
 
 
         }
@@ -138,8 +175,7 @@ var circleArray = [];
 for(i=0; i<200; i++){
     var x = Math.random()*(innerWidth-radius*2)+radius;
 var y = Math.random()*(innerHeight-radius*2)+radius;
-var dx = (Math.random()-0.5)*5;
-var dy = (Math.random()-0.5)*5;
+
 var radius = (Math.random()*20+1);
 var color1 = Math.floor(Math.random()*255).toString(16);
 var color2 = Math.floor(Math.random()*255).toString(16);
@@ -147,15 +183,15 @@ var color3 = Math.floor(Math.random()*255).toString(16);
 
 var color = '#'+color1+color2+color3;
 
-circleArray.push(new Circle(x,y,dx,dy,radius,color))
+circleArray.push(new Circle(x,y,radius,color))
 }
-var circle = new Circle(x,y,dx,dy,radius,color);
+var circle = new Circle(x,y,radius,color);
 function animate() {
   
     requestAnimationFrame(animate)
     c.clearRect(0,0,innerWidth,innerHeight);
     for(i=0; i<circleArray.length; i++){
-        circleArray[i].update();
+        circleArray[i].update(circleArray);
     }
     
     // circle.draw();
